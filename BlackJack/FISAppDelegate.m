73280d7741b7f9e160a6d809f17545d83a9ff813
eldooon <eldonchan91@gmail.com>
2016-06-17 17:38:57 -0400

    done

--- BlackJack/FISAppDelegate.m
+++ BlackJack/FISAppDelegate.m
@@ -1,11 +1,20 @@
 //  FISAppDelegate.m
 
 #import "FISAppDelegate.h"
+#import "FISBlackjackPlayer.h"
+#import "FISCardDeck.h"
 
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    FISBlackjackPlayer *eldon = [[FISBlackjackPlayer alloc]initWithName:@"Eldon"];
+    FISCardDeck *newCard = [[FISCardDeck alloc]init];
+    FISCard *card = [newCard drawNextCard];
+    [eldon acceptCard:card];
+    NSLog(@"%@", eldon.description);
+    
     return YES;
 }
 
